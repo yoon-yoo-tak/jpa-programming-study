ce220c40c20bdfb37548f29d908cd9cf858f53cf
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-03-11 21:03:23 +0900

    2022.03.11
    복습

--- ch06-model3/src/main/java/jpabook/model/myEntity/OrderItem.java
+++ ch06-model3/src/main/java/jpabook/model/myEntity/OrderItem.java
@@ -5,9 +5,6 @@ import jpabook.model.entity.Order;
 
 import javax.persistence.*;
 
-/**
- * Created by holyeye on 2014. 3. 11..
- */
 @Entity
 @Table(name = "ORDER_ITEM")
 public class OrderItem {
@@ -18,14 +15,14 @@ public class OrderItem {
 
     @ManyToOne
     @JoinColumn(name = "ITEM_ID")
-    private Item item;      //주문 상품
+    private Item item;
 
     @ManyToOne
     @JoinColumn(name = "ORDER_ID")
-    private Order order;    //주문
+    private Order order;
 
-    private int orderPrice; //주문 가격
-    private int count;      //주문 수량
+    private int orderPrice;
+    private int count;
 
     //Getter, Setter
     public Long getId() {
