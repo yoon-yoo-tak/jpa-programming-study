5731065bb4e09cb8deec354c20b06a8a5f6b9b31
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-03-12 21:52:28 +0900

    2022.03.12
    7장 학습

--- ch07-model4/src/main/java/jpabook/model/entity/item/Item.java
+++ ch07-model4/src/main/java/jpabook/model/entity/item/Item.java
@@ -7,9 +7,6 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by holyeye on 2014. 3. 11..
- */
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
