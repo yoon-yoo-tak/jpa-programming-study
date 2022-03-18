ec5a4d6203978ebc2a4639d6bac450ccfa38936b
Your Name <dbsdbxkrzz@naver.com>
2022-03-18 23:41:37 +0900

    commit test

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Address.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -21,20 +21,20 @@ public class Address {
         this.zipcode = zipcode;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
     }
 
     public void setStreet(String street) {
         this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public String getZipcode() {
